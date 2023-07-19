489680820f28e552f61970c1e651c29f1060dc6b
Fabian Preuß <preuss_fabian@gmx.de>
2023-07-19 19:17:18 +0200

    don't speed up `fastloop()` under accelerated time
    Instead of simply running the game in tighter loops, multiply resource
    calculations by game speed. This is only done for `fastloop()` for now
    as most other code is tightly build and balanced around the timings of
    `midloop()` and `longloop()`.
    
    In a few places a `++` was replaced by `4*time_multiplier` to account
    for the loop running 4 times per second and `time_multiplier` being
    0.25 under normal game speed.

--- src/functions.js
+++ src/functions.js
@@ -139,9 +139,10 @@ export function gameLoop(act){
                     global.settings.gameSpeed *= fast;
                 }
 
-                for (let key in webWorker.timers) {
-                    webWorker.timers[key] /= global.settings.gameSpeed;
-                }
+                // timers.main stays as is since ressource calculations in
+                // fastloop() are already gamespeed compensated
+                webWorker.timers.mid /= global.settings.gameSpeed;
+                webWorker.timers.long /= global.settings.gameSpeed;
 
                 if (webWorker.w){
                     webWorker.w.postMessage({ loop: 'short', period: webWorker.timers.main });
